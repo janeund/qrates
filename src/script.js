4a664f5f392fdbe3b465e301a625d3888e3b6419
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-04-30 21:10:35 +0200

    feat: update slider styles and add functionality

--- src/script.js
+++ src/script.js
@@ -1,8 +1,45 @@
 window.onload = () => {
-
+  initializeSlider();
 }
 
-const showSlides = () => {
-  let slider = document.querySelector('.slider__list');
-  let slides = document.querySelectorAll('.slider__item');
+// Active slide index
+let slideIndex = 0;
+// All slides
+let slides = document.querySelectorAll('.slider__image');
+// Interval state
+let interval = null;
+
+
+// Initialize slider
+const initializeSlider = () => { 
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add('active');
+    interval = setInterval(() => nextSlide(), 2000);
+  }
 }
+
+// Update current slide
+const updateCurrent = (index) => {  
+  slides.forEach(slide => {
+    slide.classList.remove('active')
+  });
+  slides[index].classList.add('active')
+}
+
+// Progress to last slide
+const prevSlide = () => { 
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  updateCurrent(slideIndex);
+}
+
+// Progress to next slide
+const nextSlide = () => { 
+  slideIndex++;
+  if (slideIndex === slides.length) {
+    slideIndex = 0;
+  }
+  updateCurrent(slideIndex);
+};
